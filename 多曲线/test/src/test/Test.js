4b9d134c8b898baaf614b4ab9313899d321b2975
ningbnii <296720094@qq.com>
2018-05-22 15:10:22 +0800

    多曲线update

--- 多曲线/test/src/test/Test.js
+++ 多曲线/test/src/test/Test.js
@@ -9,10 +9,20 @@ F2xExtend(test.Test, F2xContainer);
 test.Test.prototype.initUI = function () {
     var s = this;
     //f2x_auto_created_init_start
+    var _d2 = new test.refresh();
+    _d2.name = "refresh";
+    s.refresh = _d2;
+    Flash2x.d(_d2, {x: 27.95, y: 1035.5});
+    var _d1 = new test.changeBtn();
+    _d1.name = "changeBtn";
+    s.changeBtn = _d1;
+    Flash2x.d(_d1, {x: 336.3, y: 1035.5});
     var _d0 = new test.backgroundLayer();
     _d0.name = "backgroundLayer";
     s.backgroundLayer = _d0;
     s.addChild(_d0);
+    s.addChild(_d1);
+    s.addChild(_d2);
     //f2x_auto_created_init_end
 
 };
@@ -20,13 +30,31 @@ test.Test.prototype.init = function () {
     var s = this;
 
     s.numPoints = 9;
+    // 闭合状态
+    s.close = false;
     s.shape = new annie.Shape();
     s.addChild(s.shape);
+    s.textLayer = new annie.Sprite();
+    s.addChild(s.textLayer);
+
+    s.create();
+    s.refresh.addEventListener(annie.MouseEvent.CLICK, s.create.bind(this));
+    s.changeBtn.addEventListener(annie.MouseEvent.CLICK, function (e) {
+        var s = this;
+        s.close = s.close == true ? false : true;
+        s.changeBtn.btn_name.text = s.close == true ? '闭合' : '非闭合';
+    }.bind(this))
+};
+
+test.Test.prototype.create = function () {
+    var s = this;
+    s.shape.clear();
+    s.textLayer.removeAllChildren();
     // 建立由随机点构成的一个数组
     s.points = new Array();
     for (var i = 0; i < s.numPoints; i++) {
-        var tempx = Math.random() * 640;
-        var tempy = Math.random() * 1136;
+        var tempx = Math.random() * 600 + 20;
+        var tempy = Math.random() * 950 + 20;
         s.points[i] = {
             x: tempx,
             y: tempy
@@ -37,24 +65,35 @@ test.Test.prototype.init = function () {
         s.shape.endFill();
         // 绘制数字
         var text = new annie.TextField();
-        s.addChild(text);
-        text.x = tempx-5;
-        text.y = tempy-10;
+        s.textLayer.addChild(text);
+        text.x = tempx - 5;
+        text.y = tempy - 10;
         text.text = i + 1;
         text.size = 20;
     }
 
     // 绘制曲线
     s.shape.beginStroke('#000000', 1);
-    s.shape.moveTo(s.points[0].x, s.points[0].y);
-    for (var i = 1; i < s.numPoints - 2; i++) {
-        var xc = (s.points[i].x + s.points[i + 1].x) / 2;
-        var yc = (s.points[i].y + s.points[i + 1].y) / 2;
-        s.shape.quadraticCurveTo(s.points[i].x, s.points[i].y, xc, yc);
+    if (s.close) {
+        var xc1 = (s.points[0].x + s.points[s.numPoints - 1].x) / 2;
+        var yc1 = (s.points[0].y + s.points[s.numPoints - 1].y) / 2;
+        s.shape.moveTo(xc1, yc1);
+        for (var i = 0; i < s.numPoints - 1; i++) {
+            var xc = (s.points[i].x + s.points[i + 1].x) / 2;
+            var yc = (s.points[i].y + s.points[i + 1].y) / 2;
+            s.shape.quadraticCurveTo(s.points[i].x, s.points[i].y, xc, yc);
+        }
+        s.shape.quadraticCurveTo(s.points[i].x, s.points[i].y, xc1, yc1);
+    } else {
+        s.shape.moveTo(s.points[0].x, s.points[0].y);
+        for (var i = 1; i < s.numPoints - 2; i++) {
+            var xc = (s.points[i].x + s.points[i + 1].x) / 2;
+            var yc = (s.points[i].y + s.points[i + 1].y) / 2;
+            s.shape.quadraticCurveTo(s.points[i].x, s.points[i].y, xc, yc);
+        }
+        s.shape.quadraticCurveTo(s.points[i].x, s.points[i].y, s.points[i + 1].x, s.points[i + 1].y);
     }
-    s.shape.quadraticCurveTo(s.points[i].x, s.points[i].y, s.points[i + 1].x, s.points[i + 1].y);
+
     s.shape.endStroke();
-
 };
-
 
--- 多曲线/test/src/test/testAuto.js
+++ 多曲线/test/src/test/testAuto.js
@@ -11,3 +11,37 @@ test.backgroundLayer.prototype.initUI=function(){
 	Flash2x.d(_d0,{x:320,y:568});
 	s.addChild(_d0);
 };
+test.changeBtn=function(){
+	var s = this;
+	F2xContainer.call(s);
+	s.initUI();
+};
+F2xExtend(test.changeBtn,F2xContainer);
+test.changeBtn.prototype.initUI=function(){
+	var s = this;
+	var _d1=Flash2x.t(0,decodeURI("%E9%9D%9E%E9%97%AD%E5%90%88"),30,"#FFFFFF","Times New Roman",0,0,90.2,33.25,32,"left",false,false,"multiline",false);
+	_d1.name="btn_name";
+	s.btn_name=_d1;
+	Flash2x.d(_d1,{x:94.9,y:16.25});
+	var _d0=Flash2x.s({type:0,data:"A13FFIAAqJMArvAAAIAAKJg"},{type:0,color:"#000000"},null);
+	Flash2x.d(_d0,{x:140,y:32.48});
+	s.addChild(_d0);
+	s.addChild(_d1);
+};
+test.refresh=function(){
+	var s = this;
+	F2xContainer.call(s);
+	s.initUI();
+};
+F2xExtend(test.refresh,F2xContainer);
+test.refresh.prototype.initUI=function(){
+	var s = this;
+	var _d1=Flash2x.t(0,decodeURI("%E5%88%B7%E6%96%B0"),30,"#FFFFFF","Times New Roman",0,0,60.2,33.25,32,"left",false,false,"multiline",false);
+	_d1.name="btn_name";
+	s.btn_name=_d1;
+	Flash2x.d(_d1,{x:109.9,y:16.25});
+	var _d0=Flash2x.s({type:0,data:"A13FFIAAqJMArvAAAIAAKJg"},{type:0,color:"#000000"},null);
+	Flash2x.d(_d0,{x:140,y:32.48});
+	s.addChild(_d0);
+	s.addChild(_d1);
+};
