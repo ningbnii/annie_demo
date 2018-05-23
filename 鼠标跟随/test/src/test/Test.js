1eefda97e708c1403df5a515665c9ceb58cde649
ningbnii <296720094@qq.com>
2018-05-23 17:34:20 +0800

    鼠标跟随

---
+++ 鼠标跟随/test/src/test/Test.js
@@ -0,0 +1,33 @@
+var test = test || {};
+test.Test = function () {
+    var s = this;
+    F2xContainer.call(s);
+    s.initUI();
+    s.init();
+};
+F2xExtend(test.Test, F2xContainer);
+test.Test.prototype.initUI = function () {
+    var s = this;
+    //f2x_auto_created_init_start
+	var _d0=new test.backgroundLayer();
+	_d0.name="backgroundLayer";
+	s.backgroundLayer=_d0;
+	s.addChild(_d0);
+	//f2x_auto_created_init_end
+
+};
+test.Test.prototype.init = function () {
+    var s = this;
+    s.arrow = new test.Arrow();
+    s.addChild(s.arrow);
+    s.addEventListener(annie.Event.ENTER_FRAME, s.onEnterFrame.bind(this));
+
+};
+
+test.Test.prototype.onEnterFrame = function (e) {
+    var s = this;
+
+};
+
+
+
