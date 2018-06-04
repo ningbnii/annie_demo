24c5e18c3fddaf7f05f7a763c54e7866207c9862
ningbnii <296720094@qq.com>
2018-06-04 16:54:14 +0800

    高级碰撞检测

---
+++ 高级碰撞检测/test/src/test/Ball.js
@@ -0,0 +1,27 @@
+var test=test||{};
+test.Ball=function(radius,color){
+	var s = this;
+	F2xContainer.call(s);
+	s.initUI();
+	s.vx = 0;
+	s.vy = 0;
+	s.radius = radius || 40;
+	s.color = color || '#ff0000';
+	s.init();
+};
+F2xExtend(test.Ball,F2xContainer);
+test.Ball.prototype.initUI=function(){
+	var s = this;
+	//f2x_auto_created_init_start
+	
+	//f2x_auto_created_init_end
+	
+};
+test.Ball.prototype.init = function () {
+	var s = this;
+	s.shape = new annie.Shape();
+	s.addChild(s.shape);
+	s.shape.beginFill(s.color);
+	s.shape.drawCircle(0,0,s.radius);
+	s.shape.endFill();
+};
